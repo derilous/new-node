2849e521d6570e5534cf23f09b99c86edd51660f
Mohsin Habiya <mohsin.salim567@gmail.com>
2023-06-14 15:18:29 +0400

    Store page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -93,3 +93,11 @@ exports.updateStore = async (req, res) => {
   // redirect to store and tell user it worked
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({
+    slug: req.params.slug,
+  });
+  if (!store) return next();
+  res.render('store', { store, title: store.name });
+};
